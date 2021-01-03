2d4dd038f9192f42abf546ebb21e9b3a2acce3d0
azjps <azjps@users.noreply.github.com>
2021-01-03 20:17:51 +1300

    Initial cookiecutter template
    Cookiecutter template from:
    https://github.com/makupi/cookiecutter-discord.py-postgres

---
+++ bot/utils/config.py
@@ -0,0 +1,25 @@
+import json
+import os
+
+default_config = {"token": "", "prefix": "!", "database": "postgresql://localhost/postgres"}
+
+
+class Config:
+    def __init__(self, filename="config.json"):
+        self.filename = filename
+        self.config = {}
+        if not os.path.isfile(filename):
+            with open(filename, "w") as file:
+                json.dump(default_config, file)
+        with open(filename) as file:
+            self.config = json.load(file)
+        self.prefix = self.config.get("prefix", default_config.get("prefix"))
+        self.token = self.config.get("token", default_config.get("token"))
+        self.database = os.getenv("DB_DSN")  # for docker
+        if not self.database:
+            self.database = self.config.get("database", default_config.get("database"))
+
+    def store(self):
+        data = {"prefix": self.prefix, "token": self.token, "database": self.database}
+        with open(self.filename, "w") as file:
+            json.dump(data, file)
